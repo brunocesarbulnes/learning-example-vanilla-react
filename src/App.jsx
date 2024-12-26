24c5b660fb256437bb6a8a15e93d146c8a4621da
Bruno Bulnes <brunocesar.bulnes@dell.com>
2024-12-26 12:06:55 -0300

    feat: install and configure dds vanilla via cdn

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+export function App() {
+  return (
+    <>
+      <div className="dds__container">
+        <h1>Hello World DDS!</h1>
+
+        <p>
+          <button className="dds__button" type="button">Primary Button</button>
+        </p>
+
+        <p>
+          <button className="dds__button dds__button--secondary" type="button">Secondary Button</button>
+        </p>
+
+        <p>
+          <button className="dds__button dds__button--tertiary" type="button">Tertiary Button</button>
+        </p>
+      </div>
+    </>
+  )
+}
